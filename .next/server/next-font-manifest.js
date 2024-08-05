415819b843a7b71555a65c30d1883048b6088444
Promesse10 <promesseirakoze10@gmail.com>
2024-08-05 12:05:08 +0200

    first change on login

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/a12/Desktop/Team-1-project/app/layout\":[\"static/media/2b3f1035ed87a788-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
